132ad333a72f1069e31e187efbb49b186fbfd078
angl <jjose.preciado@hotmail.com>
2025-08-24 21:08:21 -0600

    first good commit

---
+++ logic_gates.v
@@ -0,0 +1,10 @@
+module logic_gates(input bit1, bit2, output _and, _nand, _or, _nor, _not, _xor, _xnor);
+    assign _and = bit1 & bit2;
+    assign _nand = !(bit1 & bit2);
+    assign _or = bit1 | bit2;
+    assign _nor = !(bit1 | bit2);
+    assign _not = !bit1;
+    assign _xor = bit1 ^ bit2;
+    assign _and = !(bit1 ^ bit2);
+
+endmodule
